20bfad087ca7ce2dd57124e54f8b6cb537eb20f9
halilintar8 <halilintar8@yahoo.com>
2017-09-09 14:22:50 +0000

    Update Dockerfile

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@ sed -i 's/#db.sql.rootUser=root/db.sql.rootUser=root/' $GTS_HOME/common.conf
 sed -i "s/#db.sql.rootPass=rootpass/db.sql.rootPass=$MYSQL_ENV_MYSQL_ROOT_PASSWORD/" $GTS_HOME/common.conf
 sed -i "s/db.sql.host=localhost/db.sql.host=$MYSQL_PORT_3306_TCP_ADDR/" $GTS_HOME/common.conf
 cd $GTS_HOME; ant all
-$GTS_HOME/bin/initdb.pl -rootPass=$MYSQL_ENV_MYSQL_ROOT_PASSWORD
+
+#$GTS_HOME/bin/initdb.pl -rootPass=$MYSQL_ENV_MYSQL_ROOT_PASSWORD
+$GTS_HOME/bin/initdb.pl -rootUser=root -rootPass=$MYSQL_ENV_MYSQL_ROOT_PASSWORD
 $GTS_HOME/bin/dbAdmin.pl -tables=ca
 $GTS_HOME/bin/admin.sh Account -account=sysadmin -nopass -create
 cp $GTS_HOME/build/*.war $CATALINA_HOME/webapps/
